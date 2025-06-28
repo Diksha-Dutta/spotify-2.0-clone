fa801dc7955bddfff4fe1ead0d65e1555e6d4744
Diksha Dutta <duttadiksha27@gmail.com>
2025-06-28 21:52:42 +0530

    🎧 Added Album page with working search + fixed context play bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,19 @@ export default function Navbar() {
       <div className="space-x-4">
         <Link to="/">Home</Link>
         <Link to="/search">Search</Link>
+        <Link to="/genre">Genre</Link>
+  <span className="relative group">
+  <button className="text-white focus:outline-none">
+    Albums 
+  </button>
+  <div className="absolute hidden group-hover:flex flex-col bg-white text-black mt-2 rounded shadow-lg min-w-[150px] z-50">
+    <Link to="/album/lofi" className="px-4 py-2 hover:bg-green-100">Lofi</Link>
+    <Link to="/album/arijit" className="px-4 py-2 hover:bg-green-100">Arijit Singh</Link>
+    <Link to="/album/workout" className="px-4 py-2 hover:bg-green-100">Workout</Link>
+    <Link to="/album/romantic" className="px-4 py-2 hover:bg-green-100">Romantic</Link>
+  </div>
+</span>
+        
        
       </div>
     </nav>
